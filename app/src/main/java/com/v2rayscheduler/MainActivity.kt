20cc22b4f59edcd0b18d2d8b2a2c8d8977349ba5
KASRA <keyvanhajahmadi4@gmail.com>
2026-07-13 21:19:42 +0330

    Fix viewModel() call outside composable in onActivityResult

--- app/src/main/java/com/v2rayscheduler/MainActivity.kt
+++ app/src/main/java/com/v2rayscheduler/MainActivity.kt
@@ -6,6 +6,7 @@ import android.net.VpnService
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,7 +16,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.v2rayscheduler.model.ScheduleConfig
 import com.v2rayscheduler.ui.MainScreen
 import com.v2rayscheduler.ui.MainViewModel
@@ -28,6 +28,8 @@ class MainActivity : ComponentActivity() {
         private const val VPN_REQUEST_CODE = 1001
     }
 
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -36,7 +38,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val viewModel: MainViewModel = viewModel()
                     val schedules by viewModel.schedules.collectAsState()
                     val connectionState by viewModel.connectionState.collectAsState()
 
@@ -97,7 +98,6 @@ class MainActivity : ComponentActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == VPN_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            val viewModel: MainViewModel = viewModel()
             viewModel.toggleConnection()
         }
     }
